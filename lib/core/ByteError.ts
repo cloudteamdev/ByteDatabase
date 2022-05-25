bd0ec260ac57b4c4f4dce32046efe1b0df0a1b7e
avon <84014668+avonryle@users.noreply.github.com>
2022-05-25 22:42:44 +0500

    Update ByteError.ts

--- lib/core/ByteError.ts
+++ lib/core/ByteError.ts
@@ -3,6 +3,6 @@ var chalk = require('chalk')
 export class ByteError extends Error {
     constructor(name: string, message: string){
         super(message)
-        this.name = `${chalk.red.bold('[ByteDatabase (Error)]:')} ${chalk.bold.yellow(name)} =>`
+        this.name = `${chalk.red.bold('[ByteDatabase (Error)]')} ${chalk.bold.magenta("=>")} ${chalk.bold.yellow(name)}`
     }
 }
