d340d4d28d8da50e4373733a24c97c694cc9a340
avon <84014668+avonryle@users.noreply.github.com>
2022-05-27 21:00:56 +0500

    Update RowManager.ts
    fixed indentation,
    added deleteRowByKey() method
    added findAllRows() method
    removed sanitization as its no longer needed in class RowManager

--- lib/structures/RowManager.ts
+++ lib/structures/RowManager.ts
@@ -1,49 +1,45 @@
-import { DEFAULT_TABLE } from "../../constants";
 import { ByteDatabase } from "../core/ByteDatabase";
-import { sanitize } from "../functions/sanitize";
 
 export class RowManager {
-    database: ByteDatabase
+    _database: ByteDatabase
     constructor(db: ByteDatabase){
-        this.database = db
+        this._database = db
     }
 
-    insertRowByKey(
-        key: string, 
-        value: any, 
-        table: string = DEFAULT_TABLE
-        ){
-            const strjson = JSON.stringify(value);
-            const q = sanitize(`INSERT INTO ${table} (ID,Json) VALUES (?,?)`);
-            return this.database.raw.prepare(q).run(key, strjson)
-        }
-
-        updateRowByKey(
-        key: string, 
-        value: any, 
-        table: string = DEFAULT_TABLE
-        ){
-            const strjson = JSON.stringify(value);
-
-            return this.database.raw
-            .prepare(`UPDATE ${table} SET Json = (?) WHERE ID = (?)`)
-            .run(key, strjson)
-            .changes
-        }
-
-        findRowByKey(
-            key: string,
-            table: string = DEFAULT_TABLE
-        ){
-            let q = sanitize(`SELECT Json FROM ${table} WHERE ID = @key`)
-            let val = this.database.raw.prepare(q).get({ key })
-            return val != null ? JSON.parse(val.Json) : null;
-        }
-
-     deleteAllRows(
-         table: string = DEFAULT_TABLE
-         ){
-            return this.database.raw.prepare(`DELETE FROM ${table}`).run().changes;
-        }
+    insertRowByKey(key: string, value: any, table: string){
+        const val = value !== "string" ? JSON.stringify(value) : value;
+        return this._database._raw.prepare(`INSERT INTO ${table} (ID,Json) VALUES (?,?)`).run(key, val)
+    }
     
+    updateRowByKey(key: string, value: any, table: string ){
+        const val = value !== "string" ? JSON.stringify(value) : value;
+        return this._database._raw.prepare(`UPDATE ${table} SET Json = (?) WHERE ID = (?)`).run(key, val)
     }
+
+    findRowByKey(key: string, table: string){
+        const val = this._database._raw.prepare(`SELECT Json FROM ${table} WHERE ID = @key`).get({ key })
+        return val !== null ? JSON.parse(val.Json) : null;
+    }
+
+    findAllRows(table: string){
+        const val = this._database._raw.prepare(`SELECT * FROM ${table}`).iterate();
+        const DataArr: object[] = [];
+        for(const i of val){
+            DataArr.push({
+                    ID: i.ID,
+                    JSON: i.Json,
+                    TABLE: table
+                })
+        }
+
+        return DataArr
+    }
+
+    deleteAllRows(table: string){
+        return this._database._raw.prepare(`DELETE FROM ${table}`).run().changes
+    }
+
+    deleteRowByKey(key: string, table: string){
+        return this._database._raw.prepare(`DELETE FROM ${table} WHERE ID = @key`).run({ key }).changes;
+    }
+}
