15a9cae5ac79cf3fc533a1b94fe98a7214738c5f
oriel-beck <orielbeck@gmail.com>
2022-05-29 19:39:32 +0300

    test should be used here, not match

--- lib/functions/createTable.ts
+++ lib/functions/createTable.ts
@@ -1,11 +1,11 @@
 import { ByteDatabase } from "../core/ByteDatabase";
 import { ByteError } from "../core/ByteError";
 
-export function createTable(name: string, db: ByteDatabase){
+export function createTable(name: string, db: ByteDatabase) {
     const regex = /[A-z0-9]+/
-    if(name.match(regex)?.[0] === name){
-    const q = `CREATE TABLE IF NOT EXISTS ${name} (ID TEXT, Json TEXT)`;
-    return db._raw.prepare(q).run()
+    if (regex.test(name)) {
+        const q = `CREATE TABLE IF NOT EXISTS ${name} (ID TEXT, Json TEXT)`;
+        return db._raw.prepare(q).run()
     } else {
         throw new ByteError("INVALID_TABLE_NAME", `${name} does not follow the regex ${regex}`)
     }
