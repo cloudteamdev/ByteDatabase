99e0cb3d1853aa05ecd3148d536b9b33a80520eb
oriel-beck <orielbeck@gmail.com>
2022-05-29 19:38:42 +0300

    formatting + path, not paths

--- lib/core/ByteDatabase.ts
+++ lib/core/ByteDatabase.ts
@@ -18,75 +18,75 @@ export class ByteDatabase extends EventEmitter<Events> {
      * ByteDatabase.raw.prepare("SQL STRING")
      * 
      */
-     public _raw: Database.Database
-     /**
-      * @property rows
-      * row handler for the database
-      */
-     private _rows: RowManager = new RowManager(this)
-     /**
-      * 
-      * @param paths
-      * path of the database file, defaulting to the memory of the database.
-      * @param props 
-      * properties for the database. check https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md#new-databasepath-options for more information.
-      * 
-      */
-     constructor(path: string = ":memory:", props: DefaultByteProperties | object = {}) {
-         super()
-         this._raw = new Database(path, props)
-         this._raw.prepare(`CREATE TABLE IF NOT EXISTS ${DEFAULT_TABLE} (ID TEXT, Json TEXT)`).run()
-     }
+    public _raw: Database.Database
+    /**
+     * @property rows
+     * row handler for the database
+     */
+    private _rows: RowManager = new RowManager(this)
+    /**
+     * 
+     * @param path
+     * path of the database file, defaulting to the memory of the database.
+     * @param props 
+     * properties for the database. check https://github.com/WiseLibs/better-sqlite3/blob/master/docs/api.md#new-databasepath-options for more information.
+     * 
+     */
+    constructor(path: string = ":memory:", props: DefaultByteProperties | object = {}) {
+        super()
+        this._raw = new Database(path, props)
+        this._raw.prepare(`CREATE TABLE IF NOT EXISTS ${DEFAULT_TABLE} (ID TEXT, Json TEXT)`).run()
+    }
 
-        public insert(key: string, val: any, table: string = DEFAULT_TABLE) {
-            if(typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "INSERT". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
-            if(val === null) throw new ByteError("MISSING_ARGUMENT", `Expected 2 arguments in method "INSERT" . \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
-            
-            if(key.includes(".")){
-                let splitted = key.split(".");
-                let ParentKey = splitted[0];
-                let got = this.find(ParentKey);
-                let _ = set(got ?? {}, splitted.slice(1).join("."), val);
-                
-                return this._rows.insertRowByKey(ParentKey, _, table);
-            } else {
-                let got = this.find(key);
-                let _ = set(got ?? {}, key, val);
+    public insert(key: string, val: any, table: string = DEFAULT_TABLE) {
+        if (typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "INSERT". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
+        if (!val) throw new ByteError("MISSING_ARGUMENT", `Expected 2 arguments in method "INSERT" . \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
 
-                return this._rows.insertRowByKey(key, _, table);
-            }
-        }
+        if (key.includes(".")) {
+            let splitted = key.split(".");
+            let ParentKey = splitted[0];
+            let got = this.find(ParentKey);
+            let _ = set(got ?? {}, splitted.slice(1).join("."), val);
 
-        public find(key: string, table: string = DEFAULT_TABLE) {
-            if(typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "INSERT". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
-            if(!key.includes(".")){
-                let result = this._rows.findRowByKey(key, table);
-                let _ = get(result, key);
-                return _
-            } else {
-                let splitted = key.split(".");
-                let ParentKey = splitted[0];
+            return this._rows.insertRowByKey(ParentKey, _, table);
+        } else {
+            let got = this.find(key);
+            let _ = set(got ?? {}, key, val);
 
-                const result = this._rows.findRowByKey(ParentKey, table);
-                let _ = get(result, splitted.slice(1).join("."));
-                return _
-            }
-        }
-
-        public all(table: string = DEFAULT_TABLE){
-            return this._rows.findAllRows(table)
-        }
-
-        public wipe(table: string = DEFAULT_TABLE) {
-           return this._rows.deleteAllRows(table)
-       }
-
-        public delete(key: string, table: string = DEFAULT_TABLE){
-            if(typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "DELETE". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`)
-            return this._rows.deleteRowByKey(key, table)
-        }
-
-        public createTable(name: string){
-            return createTable(name, this)
+            return this._rows.insertRowByKey(key, _, table);
         }
     }
+
+    public find(key: string, table: string = DEFAULT_TABLE) {
+        if (typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "INSERT". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`);
+        if (!key.includes(".")) {
+            let result = this._rows.findRowByKey(key, table);
+            let _ = get(result, key);
+            return _
+        } else {
+            let splitted = key.split(".");
+            let ParentKey = splitted[0];
+
+            const result = this._rows.findRowByKey(ParentKey, table);
+            let _ = get(result, splitted.slice(1).join("."));
+            return _
+        }
+    }
+
+    public all(table: string = DEFAULT_TABLE) {
+        return this._rows.findAllRows(table)
+    }
+
+    public wipe(table: string = DEFAULT_TABLE) {
+        return this._rows.deleteAllRows(table)
+    }
+
+    public delete(key: string, table: string = DEFAULT_TABLE) {
+        if (typeof key !== "string") throw new ByteError("INVALID_TYPE", `Expected a type of string in method "DELETE". Recieved a type of ${typeof key} instead. \n If you need help, consider joining our Discord Server!: https://join.cloudteam.me`)
+        return this._rows.deleteRowByKey(key, table)
+    }
+
+    public createTable(name: string) {
+        return createTable(name, this)
+    }
+}
