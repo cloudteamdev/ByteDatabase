a13ef62dcca655e0256b5ad7eaab079c9af28ecb
oriel-beck <orielbeck@gmail.com>
2022-05-30 21:54:47 +0300

    Made DefaultByteProperties extend better-sqlite3's Options interface

--- lib/typings/types/DBP.ts
+++ lib/typings/types/DBP.ts
@@ -1,11 +1,3 @@
-// just fyi, this is not very useful, you can just call "Options" whenever you need them, unless you plan to add more stuff to this this is not needed
-
 import { Options } from 'better-sqlite3';
 
-export type DefaultByteProperties =  {
-    readonly: Options['readonly'] 
-    fileMustExist: Options['fileMustExist']
-    timeout: Options['timeout']
-    verbose: Options['verbose']
-    nativeBinding: Options['nativeBinding']
-}
+export interface DefaultByteProperties extends Options {}
