f587252f8133e48585f2d359875a6675efed7462
avon <avonryle@cloudteam.me>
2022-06-03 21:20:07 +0500

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,55 @@
-import { ByteDatabase } from './core/ByteDatabase';
-import pkg from '../package.json'
+//core
+import Database, { Database as TypeDatabase } from 'better-sqlite3';
+import EventEmitter from '@tbnritzdoge/events';
+// types
+import { DefaultByteProperties } from './typings/interfaces/DBP';
+import { Events } from './typings/types/Events';
+// others
+import { DEFAULT_TABLE } from './util/constants';
+import { methods } from './methods/exportMethods';
 
-exports = { 
-    ByteDatabase,
-    version: pkg.version 
+export class ByteDatabase extends EventEmitter<Events> {
+  public _raw: TypeDatabase;
+  public options: DefaultByteProperties;
+
+  constructor(
+    path: string,
+    options: DefaultByteProperties = {
+      sanitize: true,
+      path: ':memory:',
+    },
+  ) {
+    super();
+
+    options.path = path;
+
+    this.options = options;
+    this._raw = new Database(path, options);
+
+    this._raw
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS ${DEFAULT_TABLE} (ID TEXT, Json TEXT)`,
+      )
+      .run();
+  }
+
+  public insert(key: string, val: any, table: string = DEFAULT_TABLE) {
+    return methods.insert(key, val, table, this);
+  }
+
+  public find(key: string, table: string = DEFAULT_TABLE) {
+    return methods.find(key, table, this);
+  }
+
+  public all(table: string = DEFAULT_TABLE) {
+    return methods.all(table, this);
+  }
+
+  public wipe(table: string = DEFAULT_TABLE) {
+    return methods.wipe(table, this);
+  }
+
+  public delete(key: string, table: string = DEFAULT_TABLE) {
+    return methods.delete(key, table, this);
+  }
 }
