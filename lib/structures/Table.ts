d79a0d2681da5f805e83a5658700f77d4e4013b8
avon <avonryle@cloudteam.me>
2022-06-07 23:49:28 +0500

    add method init

--- lib/structures/Table.ts
+++ lib/structures/Table.ts
@@ -3,16 +3,24 @@ import { CREATE_TABLE_REGEX } from '../util/constants';
 import { ByteError } from './ByteError';
 
 export class Table {
+  private _database: ByteDatabase;
+  private _tableName: string;
+
   constructor(name: string, db: ByteDatabase) {
-    if (CREATE_TABLE_REGEX.test(name)) {
-      return db._raw
-        .prepare(`CREATE TABLE IF NOT EXISTS ${name} (ID TEXT, Json TEXT)`)
-        .run();
-    } else {
-      throw new ByteError(
-        'INVALID_TABLE_NAME',
-        `Table name ${name} does not follow the regex ${CREATE_TABLE_REGEX}. Table names accepts only alphanumerical values.`,
-      );
-    }
+    this._database = db;
+    this._tableName = name;
+  }
+
+  init() {
+    return CREATE_TABLE_REGEX.test(this._tableName)
+      ? this._database._raw
+          .prepare(
+            `CREATE TABLE IF NOT EXISTS ${this._tableName} (ID TEXT, Json TEXT)`,
+          )
+          .run()
+      : new ByteError(
+          'INVALID_TABLE_NAME',
+          `Table name ${this._tableName} does not follow the regex ${CREATE_TABLE_REGEX}. Table names accepts only alphanumerical values.`,
+        );
   }
 }
