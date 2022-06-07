96a44b6369ab767547ce998c28c590fc4d39cc75
avon <avonryle@cloudteam.me>
2022-06-07 23:50:26 +0500

    now sanitizes inputs only

--- lib/structures/RowManager.ts
+++ lib/structures/RowManager.ts
@@ -11,41 +11,39 @@ export class RowManager {
 
   insertRowByKey(
     key: string,
-    value: Record<string, unknown> | string | unknown[],
+    value: Record<string, unknown> | string | number | unknown[],
     table: string = DEFAULT_TABLE,
   ) {
-    const val = value !== 'string' ? JSON.stringify(value) : value;
-    const query = this._database.options.sanitize
-      ? sanitize(`INSERT INTO ${table} (ID,Json) VALUES (?,?)`)
-      : `INSERT INTO ${table} (ID,Json) VALUES (?,?)`;
-    return this._database._raw.prepare(query).run(key, val);
+    value = value !== 'string' ? JSON.stringify(value) : value;
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    return this._database._raw
+      .prepare(`INSERT INTO ${table} (ID,Json) VALUES (?,?)`)
+      .run(key, value);
   }
 
   updateRowByKey(
     key: string,
-    value: Record<string, unknown> | string | unknown[],
+    value: Record<string, unknown> | string | number | unknown[],
     table: string = DEFAULT_TABLE,
   ) {
-    const val = value !== 'string' ? JSON.stringify(value) : value;
-    const query = this._database.options.sanitize
-      ? sanitize(`UPDATE ${table} SET Json = (?) WHERE ID = (?)`)
-      : `UPDATE ${table} SET Json = (?) WHERE ID = (?)`;
-    return this._database._raw.prepare(query).run(key, val);
+    value = value !== 'string' ? JSON.stringify(value) : value;
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    return this._database._raw
+      .prepare(`UPDATE ${table} SET Json = (?) WHERE ID = (?)`)
+      .run(key, value);
   }
 
   findRowByKey(key: string, table: string = DEFAULT_TABLE) {
-    const query = this._database.options.sanitize
-      ? sanitize(`SELECT Json FROM ${table} WHERE ID = @key`)
-      : `SELECT Json FROM ${table} WHERE ID = @key`;
-    const val = this._database._raw.prepare(query).get({ key });
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    const val = this._database._raw
+      .prepare(`SELECT Json FROM ${table} WHERE ID = @key`)
+      .get({ key });
     return val != null ? JSON.parse(val.Json) : null;
   }
 
   findAllRows(table: string = DEFAULT_TABLE) {
-    const query = this._database.options.sanitize
-      ? sanitize(`SELECT * FROM ${table}`)
-      : `SELECT * FROM ${table}`;
-    const val = this._database._raw.prepare(query).iterate();
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    const val = this._database._raw.prepare(`SELECT * FROM ${table}`).iterate();
     const data: object[] = [];
     for (const i of val) {
       data.push({
@@ -59,16 +57,14 @@ export class RowManager {
   }
 
   deleteAllRows(table: string = DEFAULT_TABLE) {
-    const query = this._database.options.sanitize
-      ? sanitize(`DELETE FROM ${table}`)
-      : `DELETE FROM ${table}`;
-    return this._database._raw.prepare(query).run();
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    return this._database._raw.prepare(`DELETE FROM ${table}`).run();
   }
 
   deleteRowByKey(key: string, table: string = DEFAULT_TABLE) {
-    const query = this._database.options.sanitize
-      ? sanitize(`DELETE FROM ${table} WHERE ID = @key`)
-      : `DELETE FROM ${table} WHERE ID = @key`;
-    return this._database._raw.prepare(query).run({ key });
+    table = this._database.options.sanitize ? sanitize(table) : table;
+    return this._database._raw
+      .prepare(`DELETE FROM ${table} WHERE ID = @key`)
+      .run({ key });
   }
 }
