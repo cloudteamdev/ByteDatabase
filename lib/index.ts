6f9a20d5541478a2bc330316ba5aae07b9128914
avon <avonryle@cloudteam.me>
2022-06-07 23:48:21 +0500

    exports table, path is now optional

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,14 @@
 //core
 import Database, { Database as TypeDatabase } from 'better-sqlite3';
 import EventEmitter from '@tbnritzdoge/events';
+import { Table } from './structures/Table';
 // types
 import { DefaultByteProperties } from './typings/interfaces/DBP';
 import { Events } from './typings/types/Events';
 // others
 import { DEFAULT_TABLE } from './util/constants';
 import { methods } from './methods/exportMethods';
+import { findTables } from './functions/findTables';
 
 export class ByteDatabase extends EventEmitter<Events> {
   public _raw: TypeDatabase;
@@ -16,21 +18,22 @@ export class ByteDatabase extends EventEmitter<Events> {
     path: string,
     options: DefaultByteProperties = {
       sanitize: true,
-      path: ':memory:',
     },
   ) {
     super();
 
-    options.path = path;
-
+    path ??= ":memory:"
     this.options = options;
     this._raw = new Database(path, options);
-
     this._raw
       .prepare(
         `CREATE TABLE IF NOT EXISTS ${DEFAULT_TABLE} (ID TEXT, Json TEXT)`,
       )
       .run();
+  }
+
+  public findTables() {
+    return findTables(this);
   }
 
   public insert(key: string, val: any, table: string = DEFAULT_TABLE) {
@@ -53,3 +56,5 @@ export class ByteDatabase extends EventEmitter<Events> {
     return methods.delete(key, table, this);
   }
 }
+
+export { Table };
