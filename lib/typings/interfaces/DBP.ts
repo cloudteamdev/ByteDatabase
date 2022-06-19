55a2e4494d29c8dc580b4b3c420395516aff84ef
avon <avonryle@cloudteam.me>
2022-06-19 23:12:55 +0500

    feat: v2 rewrite

--- lib/typings/interfaces/DBP.ts
+++ lib/typings/interfaces/DBP.ts
@@ -1,5 +1,9 @@
 import { Options } from 'better-sqlite3';
+import { If } from '../types';
 
-export interface DefaultByteProperties extends Options {
-    sanitize: boolean
+export interface DefaultByteProperties<T extends boolean = boolean>
+  extends Options {
+  sanitize: If<T, boolean, null>;
+  logExecutionTime: If<T, boolean, null>;
+  colorizeLoggedMessages: If<T, boolean, null>;
 }
